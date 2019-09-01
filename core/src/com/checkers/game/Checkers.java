7f883b4bdf53fc891e4e4d4430d32c7f0e0e86ea
Clayton Davidson <cldavidson@cn.edu>
2019-08-31 20:40:04 -0400

    Initial commit

---
+++ core/src/com/checkers/game/Checkers.java
@@ -0,0 +1,16 @@
+package com.checkers.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Checkers extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+}
