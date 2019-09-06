853ffbe38003a77f5f18588116594d52470e32b7
Xulder <sjjplayer@gmail.com>
2019-09-05 23:03:20 -0700

    Set up rendering, assetmanager, and created assets.

--- core/src/com/checkers/game/Checkers.java
+++ core/src/com/checkers/game/Checkers.java
@@ -1,77 +1,81 @@
 package com.checkers.game;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.Array;
-
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Checkers extends ApplicationAdapter {
-	/*Asset variables*/
-	private Texture boardImage;
-	private Texture whitePieceImage;
-	private Texture blackPieceImage;
-	private SpriteBatch batch;
+	private AssetManager manager;
+	private FileHandleResolver resolver;
+	private ArrayList<Texture> redPieces;
+	private ArrayList<Texture> whitePieces;
+	private TiledMap board;
+	private TiledMapTileSet tileSet;
 	private OrthographicCamera camera;
-	private Rectangle board;
-	private Array<Rectangle> whitePieces;
-	private Array<Rectangle> blackPieces;
-	private Array<Rectangle> boardSquares;
-	
-
-	
+	private TiledMapRenderer renderer;
 
     @Override
     public void create() {
-    	// load images for board and pieces, board 512x512, pieces 64x64
-    	boardImage = new Texture(Gdx.files.internal("board.png"));
-    	//whitePieceImage = new Texture(Gdx.files.internal("whitePiece.png"));
-    	//blackPieceImage = new Texture(Gdx.files.internal("blackPieces.png"));
-    	
-    	// create camera and SpriteBatch
     	camera = new OrthographicCamera();
-    	camera.setToOrtho(false, 600, 600);
-    	batch = new SpriteBatch();
+    	camera.setToOrtho(false, 8, 8);
+    	camera.update();
     	
-    	// create rectangle to represent the board
-    	board = new Rectangle();
-    	board.x = 600 / 2 - 512 / 2; // center board horizontally
-    	board.y = 600 / 2 - 512 / 2; // center board vertically
-    	board.width = 512;
-    	board.height = 512;
+    	manager = new AssetManager();
+    	loadAssets();
     	
-    	//#TODO: create board spaces
+    	board = manager.get("board.tmx");
+    	tileSet = board.getTileSets().getTileSet(0);
+    	renderer = new OrthogonalTiledMapRenderer(board, 1 / 64f);
+//    	//Find out how to render pieces as just other tiles
+//    	TiledMapTileLayer boardLayer = (TiledMapTileLayer) board.getLayers().get(0);
+//    	TiledMapTileLayer pieceLayer = (TiledMapTileLayer) board.getLayers().get(1);
+//    	for(int x = 1; x < 8; x++) {
+//    		for(int y = 1; y < 8; y++) {
+//    			if(boardLayer.getCell(x, y).getTile().getId() == 1) {
+//    				if(y <= 3) {
+//						pieceLayer.getCell(x, y).setTile(tileSet.getTile(2)); //Gives null pointer for some reason
+//	    			} else if(y >= 6) {
+//						pieceLayer.getCell(x, y).setTile(tileSet.getTile(3));
+//    				}
+//    			}
+//			}
+//		}
+	}
     	
-    	//#TODO: create the piece arrays and set them to start positions
-    	
-    }
 
     @Override
     public void render() {
-    	// clear the screen with grey
-    	Gdx.gl20.glClearColor(0.2f, 0.2f, 0.2f, 1);
-    	Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    	
-    	// update camera matrices
+    	Gdx.gl.glClearColor(0, 0, 0, 1);
+    	Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+    	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     	camera.update();
+    	renderer.setView(camera);
+    	int[] bg = {1};
+    	int[] fg = {0};
+    	renderer.render(bg);
+    	renderer.render(fg);
     	
-    	// render SpriteBatch relative to camera
-    	batch.setProjectionMatrix(camera.combined);
-    	
-    	// begin new batch and draw the board and all pieces
-    	batch.begin();
-    	batch.draw(boardImage, board.x, board.y);
-    	batch.end();
     }
     
-    @Override
-    public void dispose() {
-    	// dispose of all resources
-    	boardImage.dispose();
+    private void loadAssets() {
+    	manager.load("red.png", Texture.class);
+    	manager.load("white.png", Texture.class);
+    	manager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+    	manager.load("board.tmx", TiledMap.class);
+    	manager.finishLoading();
     }
 }
